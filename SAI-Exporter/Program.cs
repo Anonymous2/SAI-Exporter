9ad221b2c57d8b667b545fffbb6d5e8b97b7ea1d
Discover- <amort11@hotmail.com>
2013-12-09 10:25:14 +0100

    Fix an issue where there would be SQL errors because comments or creature/gameobject names would contain upper comma's. Also get rid of some unused DB methods

--- SAI-Exporter/Program.cs
+++ SAI-Exporter/Program.cs
@@ -119,13 +119,15 @@ namespace SAI_Exporter
                         {
                             SmartScript smartScript = smartScripts[i];
 
+                            string comment = smartScript.comment.Replace('"', '\'');
+
                             fullLine += "(" + entryOrGuidSET + "," + smartScript.source_type + "," + smartScript.id + "," + smartScript.link + "," + smartScript.event_type + "," +
                                                             smartScript.event_phase_mask + "," + smartScript.event_chance + "," + smartScript.event_flags + "," + smartScript.event_param1 + "," +
                                                             smartScript.event_param2 + "," + smartScript.event_param3 + "," + smartScript.event_param4 + "," + smartScript.action_type + "," +
                                                             smartScript.action_param1 + "," + smartScript.action_param2 + "," + smartScript.action_param2 + "," + smartScript.action_param4 + "," +
                                                             smartScript.action_param5 + "," + smartScript.action_param6 + "," + smartScript.target_type + "," + smartScript.target_param1 + "," +
                                                             smartScript.target_param2 + "," + smartScript.target_param3 + "," + smartScript.target_x + "," + smartScript.target_y + "," +
-                                                            smartScript.target_z + "," + smartScript.target_o + "," + '"' + smartScript.comment + '"' + ")";
+                                                            smartScript.target_z + "," + smartScript.target_o + "," + '"' + comment + '"' + ")";
 
                             if (i != smartScripts.Count - 1)
                                 fullLine += ",\n";
