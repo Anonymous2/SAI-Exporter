1899f163b047f2c7f575db62c6826f1a058e129e
Discover- <amort11@hotmail.com>
2013-12-09 09:39:39 +0100

    Implement a progress bar as well as UPDATE queries for the ainames

--- SAI-Exporter/Program.cs
+++ SAI-Exporter/Program.cs
@@ -6,8 +6,11 @@ using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using SAI_Exporter;
+
 using MySql.Data.MySqlClient;
 using Nito.AsyncEx;
+using System.Collections.Specialized;
 
 namespace SAI_Exporter
 {
@@ -22,18 +25,24 @@ namespace SAI_Exporter
         {
         WriteSqlInformation:
             Console.WriteLine("SQL Information:");
-            Console.Write("Host: ");
-            string host = Console.ReadLine();
-            Console.Write("User: ");
-            string user = Console.ReadLine();
-            Console.Write("Pass: ");
-            string pass = Console.ReadLine();
-            Console.Write("World DB: ");
-            string worldDB = Console.ReadLine();
-            Console.Write("Port: ");
-            string portStr = Console.ReadLine();
+            //Console.Write("Host: ");
+            //string host = Console.ReadLine();
+            //Console.Write("User: ");
+            //string user = Console.ReadLine();
+            //Console.Write("Pass: ");
+            //string pass = Console.ReadLine();
+            //Console.Write("World DB: ");
+            //string worldDB = Console.ReadLine();
+            //Console.Write("Port: ");
+            //string portStr = Console.ReadLine();
+            string host = "localhost";
+            string user = "root";
+            string pass = "123";
+            string worldDB = "trinitycore_world";
+            string portStr = "3306";
 
             UInt32 port;
+
             if (!UInt32.TryParse(portStr, out port))
             {
                 Console.WriteLine("The specified port could not be converted to an unsigned integer. Please try again.");
@@ -56,8 +65,15 @@ namespace SAI_Exporter
 
                 using (var outputFile = new StreamWriter("output.sql", true))
                 {
-                    foreach (SmartScript smartScriptDistinct in smartScriptsDistinct)
+                    for (int j = 0; j < smartScriptsDistinct.Count; ++j)
                     {
+                        SmartScript smartScriptDistinct = smartScriptsDistinct[j];
+
+                        decimal _j = (decimal)j;
+                        decimal _count = (decimal)smartScriptsDistinct.Count;
+                        decimal pct = (_j / _count) * 100;
+                        ProgressBarHelper.RenderConsoleProgress((int)pct, '\u2590', Console.ForegroundColor, ((int)pct).ToString() + "%");
+
                         string fullLine = String.Empty;
                         int entryorguid = smartScriptDistinct.entryorguid;
                         SourceTypes sourceType = (SourceTypes)smartScriptDistinct.source_type;
@@ -76,6 +92,23 @@ namespace SAI_Exporter
                         }
                         else
                             fullLine += "SET @ENTRY := " + smartScriptDistinct.entryorguid + ";\n";
+
+                        switch ((SourceTypes)smartScriptDistinct.source_type)
+                        {
+                            case SourceTypes.SourceTypeCreature:
+                                fullLine += "UPDATE `creature_template` SET `AIName`=" + '"' + "SmartAI" + '"' + " WHERE `entry`=" + entryOrGuidSET + ";\n";
+                                break;
+                            case SourceTypes.SourceTypeGameobject:
+                                fullLine += "UPDATE `gameobject_template` SET `AIName`=" + '"' + "SmartGameObjectAI" + '"' + " WHERE `entry`=" + entryOrGuidSET + ";\n";
+                                break;
+                            case SourceTypes.SourceTypeAreaTrigger:
+                                fullLine += "DELETE FROM `areatrigger_scripts` WHERE `entry`=" + entryOrGuidSET + ";\n";
+                                fullLine += "INSERT INTO `areatrigger_scripts` VALUES (" + entryOrGuidSET + "," + '"' + "SmartTrigger" + '"' + ");\n";
+                                break;
+                            case SourceTypes.SourceTypeNone:
+                            case SourceTypes.SourceTypeScriptedActionlist:
+                                break;
+                        }
 
                         fullLine += "DELETE FROM `smart_scripts` WHERE `entryorguid`=" + entryOrGuidSET + " AND `source_type`=" + smartScriptDistinct.source_type + ";\n";
                         fullLine += "INSERT INTO `smart_scripts` (`entryorguid`,`source_type`,`id`,`link`,`event_type`,`event_phase_mask`,`event_chance`,`event_flags`,`event_param1`,`event_param2`,`event_param3`,`event_param4`,`action_type`,`action_param1`,`action_param2`,`action_param3`,`action_param4`,`action_param5`,`action_param6`,`target_type`,`target_param1`,`target_param2`,`target_param3`,`target_x`,`target_y`,`target_z`,`target_o`,`comment`) VALUES\n";
